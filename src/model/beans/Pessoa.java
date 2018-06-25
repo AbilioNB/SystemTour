da99b433e67bb29347dd85dea04e8d75caf30ae6
gvgabriellb <gv.gabriellb@gmail.com>
2018-06-24 23:54:43 -0300

    caracteristicas de pessoa
    coloquei algumas só para agt ter uma ideia

--- src/model/beans/Pessoa.java
+++ src/model/beans/Pessoa.java
@@ -1,4 +1,11 @@
 package model.beans;
 
 public class Pessoa {
+    private String nome;
+    private String email;
+    private String CPF;
+    private String usuario;
+    private String senha;
+    private int idade;// se precisar da data em si dps é só alterar para Date :D
+
 }
