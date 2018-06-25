4c7325a96e7e6c7a8004131f76ff36df3ee9b33f
gvgabriellb <gv.gabriellb@gmail.com>
2018-06-24 23:31:44 -0300

    teste

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Turismo Control");
+        primaryStage.setTitle("Turismo Control sei lá");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
