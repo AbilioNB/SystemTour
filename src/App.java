198c11913c300c1968b5070f08062f6453996910
[AbilioNB] <[abilionbarros@gmail.com]>
2018-07-05 19:43:11 -0300

    remove --all

--- src/App.java
+++
@@ -1,6 +0,0 @@
-import javax.swing.*;
-
-public class App {
-    private JRadioButton radioButton1;
-    private JPanel panel1;
-}
--- src/model/beans/Administrador.java
+++
@@ -1,7 +0,0 @@
-package model.beans;
-
-public class Administrador extends Pessoa {
-    private String login;
-    private String senha;
-    private int id;
-}
