11ab38e69d20c2cb548fca536b1c4fb4d71323cb
[AbilioNB] <[abilionbarros@gmail.com]>
2018-07-06 13:31:34 -0300

    attes2

--- SystemTour/src/Controllers/CadastrarController.java
+++ SystemTour/src/Controllers/CadastrarController.java
@@ -1,15 +1,32 @@
 package Controllers;
 
-
+import javafx.scene.control.TextField;
 import javafx.fxml.FXML;
+import model.beans.Cliente;
 import sample.Main;
 
+import java.util.HashMap;
+
+
+
 public class CadastrarController {
+
+    @FXML
+    private TextField campoNome;
+
+
     @FXML
     protected void buttonAction(javafx.event.ActionEvent actionEvent) {
         System.out.println("Menu");
         Main.trocaTela("Entrar");
-
-
+        String nome = campoNome.getText();
+        System.out.println(nome);
     }
+   /* //receer dados
+     String nome = campoNome.getText();*/
+
+
+
+
+
 }
