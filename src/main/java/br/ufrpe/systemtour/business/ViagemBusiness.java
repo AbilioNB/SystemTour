702322726f12d2849458581d92aef1beea5cd1be
[AbilioNB] <[abilionbarros@gmail.com]>
2018-08-08 00:53:45 -0300

    remover comentario posteriormente

--- src/main/java/br/ufrpe/systemtour/business/ViagemBusiness.java
+++ src/main/java/br/ufrpe/systemtour/business/ViagemBusiness.java
@@ -39,7 +39,7 @@ public class ViagemBusiness {
             validarOrigemDestino(viagem);
             viagem.setAtivo(1);
             ViagensDAO.salvarViagem(viagem);
-            ViagensDAO.putBd();
+            //ViagensDAO.putBd();
 
         }catch (NullPointerException e){
             vv.mensagemErroData();
